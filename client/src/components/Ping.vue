f1d9a89cbf08d5f3f1b9ed065ce6a7bd721c5222
Khail Saboor <khalilsaboor1@gmail.com>
2018-10-29 00:10:49 -0400

    first one

---
+++ client/src/components/Ping.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+    export default {
+        name:'Ping',
+        data(){
+            return{
+                msg: '',
+            };
+        },
+        methods: {
+            getmessage() {
+                const path = 'http://localhost:5000/ping';
+                axios.get(path)
+                .then((res) => {
+                    this.msg = res.data;
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+            },
+        },
+        created() {
+            this.getmessage();
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
